a037530e66f976368ac16d1bf8e5b02298c1e8ce
bradychen <bradychen2@gmail.com>
2021-04-27 19:09:27 +0800

    feat: set tickets view page

--- app.js
+++ app.js
@@ -10,12 +10,34 @@ app.engine('hbs', exphbs({
 
 app.set('view engine', 'hbs')
 
+// Homepage
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+// Movies & Auditoriums page
 app.get('/movies', (req, res) => {
   res.render('moviesAuditoriums')
+})
+
+// Tickets page
+app.get('/tickets', (req, res) => {
+  res.render('tickets')
+})
+
+// Theaters page
+app.get('/theaters', (req, res) => {
+  res.render('theaters')
+})
+
+// Customers page
+app.get('/customers', (req, res) => {
+  res.render('customers')
+})
+
+// Projector page
+app.get('/projectors', (req, res) => {
+  res.render('projectorEquipments')
 })
 
 app.listen(port, () => {
