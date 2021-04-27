82b8be75d62feac7ae70e93681d8f9b1e112d0e2
bradychen <bradychen2@gmail.com>
2021-04-27 14:20:10 +0800

    project init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: 'main'
+}))
+
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`App is listening on http://localhost:${port}`)
+})
