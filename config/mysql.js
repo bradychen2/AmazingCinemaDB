1f45ea6725043363d7f0819496a0f5d75a38ca06
bradychen2 <62660250+bradychen2@users.noreply.github.com>
2021-05-20 23:52:00 +0800

    modify mysql.js

--- config/mysql.js
+++ config/mysql.js
@@ -3,7 +3,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   host: 'classmysql.engr.oregonstate.edu',
   user: 'cs340_chengpo',
-  password: '4241',
+  password: '****',
   database: 'cs340_chengpo'
 })
 
